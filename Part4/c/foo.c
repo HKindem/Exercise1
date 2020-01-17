800c12f798ce1e854e7cad9d4a02d7d6a83c0057
Haakon <haakon@kindem.nu>
2020-01-17 14:22:26 +0100

    Should be a working C example

--- Part4/c/foo.c
+++ Part4/c/foo.c
@@ -1,22 +1,35 @@
 #include <pthread.h>
 #include <stdio.h>
 
+
+
+pthread_t incrementingThread;
+pthread_t decrementingThread; 
 int i = 0;
 
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
-    return NULL;
+    for ( int x = 1; x <= 1000000 ; x++ )
+        {
+            i++;
+        }
 }
 
 void* decrementingThreadFunction(){
-    // TODO: decrement testi 1_000_000 times
-    return NULL;
+    // TODO: decrement testi 1_000_000
+        for ( int x = 1; x <= 1000000 ; x++ )
+        {
+            i--;
+        } 
 }
 
 
 int main(){
     // TODO: declare incrementingThread and decrementingThread (hint: google pthread_create)
+ 
+
+
     pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
     pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
     
